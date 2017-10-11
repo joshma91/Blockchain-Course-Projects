6845b5c65ece6a1161705fd92e32e768256fd189
Josh Ma <josh.ma91@gmail.com>
2017-10-11 15:44:36 -0700

    Finished BiddingContract.sol

--- contracts/BiddingContract.sol
+++ contracts/BiddingContract.sol
@@ -12,12 +12,14 @@ pragma solidity ^0.4.4;
 
 contract BiddingContract {
 
-  bytes32   name;
+  string    name;
   string    description;
   uint      duration;
   uint      startBid;
 
   uint      createdAt;
+  address   owner;
+  bool      ended;
 
   struct Bidder {
     address   bidder;
@@ -28,18 +30,18 @@ contract BiddingContract {
   }
 
   // Declare the events
-  event HighBidChanged(address addr, bytes32 nm, uint  newHighBid);
-  event BidFailed(address addr, bytes32 nm, uint bidAmt);
+  event HighBidChanged(address addr, string nm, uint  newHighBid);
+  event BidFailed(address addr, string nm, uint bidAmt);
 
   // Maintain all bidders in an array
-  mapping(address=>Bidder) biddersMapping;
+  mapping(address=>Bidder) losersMapping;
 
   // This maintains the high bidder
   Bidder    highBidder;
 
   // duration in minutes
   // start price in ethers
-  function BiddingContract(bytes32 nm, string desc, uint dur, uint sBid) {
+  function BiddingContract(string nm, string desc, uint dur, uint sBid) {
     // constructor
     name=nm;
     description=desc;
@@ -47,37 +49,43 @@ contract BiddingContract {
     startBid=sBid;
     // Initalize createdAt to current time
     createdAt = now;
-
+    owner = msg.sender;
   }
 
   // Bid function is what gets called by any bidder
-  function  placeBid(string name) payable {
+  function  placeBid(string bidderName) payable {
 
     uint bid = msg.value;
     // check if the duration has not expired
     // if it has then throw an exception
-    if((createdAt*1 minutes + duration) < now*1 minutes) revert();
+    if(didBidEnd()) revert();
 
     // currentBid = highBidder.bidAmount
-    currentBid = highBidder.bidAmount;
+    uint currentBid = highBidder.bidAmount;
     //When the bid is too small emit a BidFailed event
     if(bid <= currentBid) {
       BidFailed(msg.sender, name, bid);
       revert();
-    }
+    } else {
 
-    // Create a Bidder structure and add to bidders array
-    biddersMapping[msg.sender].bidder = msg.sender;
-    biddersMapping[msg.sender].name = name;
-    biddersMapping[msg.sender].bidAmount = bid;
-    biddersMapping[msg.sender].claimedEthers = false;
+      //since the current bid is greater, we put the old highBidder into the losers array
+      if(highBidder.bidAmount != 0){
+        /*losersMapping[highBidder.bidder].bidder = highBidder.bidder;
+        losersMapping[highBidder.bidder].name = highBidder.name;
+        losersMapping[highBidder.bidder].bidAmount = highBidder.bid;
+        losersMapping[highBidder.bidder].claimedEthers = highBidder.claimedEthers;*/
 
-    // If bid > currentBid add the current high bidder to bidders array
-    // Replace the highBidder with the new high bidder
-    if(bid > currentBid){
-      highBidder = biddersMapping[msg.sender];
-        // emit High bid event
-        HighBidChanged(highBidder.bidder, highBidder.name, highBidder.bidAmount);
+        losersMapping[highBidder.bidder] = highBidder;
+      }
+
+      //assign current bidder to high bidder;
+      highBidder.bidder = msg.sender;
+      highBidder.name = bidderName;
+      highBidder.bidAmount = bid;
+      highBidder.claimedEthers = false;
+
+      // emit High bid event
+      HighBidChanged(highBidder.bidder, highBidder.name, highBidder.bidAmount);
     }
   }
 
@@ -91,31 +99,54 @@ contract BiddingContract {
   // Losers will be added to the bidders array ... the claim flag in struct
   // maintains the status of whether the caller has already been given the ethers or not
   function  getClaimAmount() returns(uint){
+
     // check if msg.sender in the bidders`
     // check if claims flag is FALSE
     // return the bid amount
+    if(losersMapping[msg.sender].bidAmount == 0 ||
+      losersMapping[msg.sender].claimedEthers == true) {
+        return 0;
+      } else {
+        return losersMapping[msg.sender].bidAmount;
+      }
+
   }
 
   // Losers will call this to get their bid ethers back
   function claimEthers() {
     // Check if caller is eligible for clain - getClaimAmout
     // If they are then send ethers to them
-    // remove the bidder from the bidders
+    // set the claimedEthers flag to true
+    uint sendAmt = getClaimAmount();
+    if(sendAmt > 0){
+      assert(msg.sender.send(sendAmt));
+      losersMapping[msg.sender].claimedEthers = true;
+      return;
+    }
+    //if address not found, bidamt is 0, or already claimed
+    revert();
   }
 
   // Can a bid end if there are unclaimed ethers
   // In later version the claims data will be moved to a separate contract
   // Claims will be made losers against the separate contract
-  function  canBidEnd() returns (bool) {
+  function  didBidEnd() returns (bool) {
     // bidders.length == 0 for bid to end
     // check if the bidding is active i.e., not expired
+    if(ended == true ||
+      ((createdAt*1 minutes + duration) < now*1 minutes)) return true;
+  }
+
+  modifier OwnerOnly(){
+    if(msg.sender == owner) _;
+    else revert();
   }
 
   // This ends the bidding
   // Only owner can call this function - apply modifier
   // All ethers returned to the owner as part of self destruct
-  function endBidding() {
-
+  function endBidding() OwnerOnly {
+      ended = true;
   }
 
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,6 @@
 var BiddingContract = artifacts.require("./BiddingContract.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(BiddingContract);
-  
+  deployer.deploy(BiddingContract, "Josh", "Josh's auction", 100, 1);
+
 };
