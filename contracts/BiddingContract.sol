12781c99b7eef6b2e0c735bb9a61cb58d3c7d9e7
Josh Ma <josh.ma91@gmail.com>
2017-10-11 13:24:40 -0700

    WIP

--- contracts/BiddingContract.sol
+++ contracts/BiddingContract.sol
@@ -32,44 +32,58 @@ contract BiddingContract {
   event BidFailed(address addr, bytes32 nm, uint bidAmt);
 
   // Maintain all bidders in an array
-  Bidder[]  bidders;
+  mapping(address=>Bidder) biddersMapping;
 
   // This maintains the high bidder
   Bidder    highBidder;
 
   // duration in minutes
   // start price in ethers
-  function BiddingContract(bytes32 nm, 
-                           string desc, 
-                           uint dur, 
-                           uint sBid) {
+  function BiddingContract(bytes32 nm, string desc, uint dur, uint sBid) {
     // constructor
     name=nm;
     description=desc;
     duration=dur;
     startBid=sBid;
     // Initalize createdAt to current time
+    createdAt = now;
+
   }
 
   // Bid function is what gets called by any bidder
-  function  placeBid(string name, uint bid) payable {
+  function  placeBid(string name) payable {
 
+    uint bid = msg.value;
     // check if the duration has not expired
     // if it has then throw an exception
+    if((createdAt*1 minutes + duration) < now*1 minutes) revert();
 
     // currentBid = highBidder.bidAmount
-    // If bid <= currentBid    // Consider the case when there is no bid
-    // Emit a BidFailed Event
-    // Create a Bidder structure and add to bidders array
+    currentBid = highBidder.bidAmount;
+    //When the bid is too small emit a BidFailed event
+    if(bid <= currentBid) {
+      BidFailed(msg.sender, name, bid);
+      revert();
+    }
 
-    // If bid > currentBid
-    // Add the current high bidder to bidders array
+    // Create a Bidder structure and add to bidders array
+    biddersMapping[msg.sender].bidder = msg.sender;
+    biddersMapping[msg.sender].name = name;
+    biddersMapping[msg.sender].bidAmount = bid;
+    biddersMapping[msg.sender].claimedEthers = false;
+
+    // If bid > currentBid add the current high bidder to bidders array
     // Replace the highBidder with the new high bidder
-    // emit High bid event
+    if(bid > currentBid){
+      highBidder = biddersMapping[msg.sender];
+        // emit High bid event
+        HighBidChanged(highBidder.bidder, highBidder.name, highBidder.bidAmount);
+    }
   }
 
   function getHighBid() constant returns(uint){
     // Return the bid amount held in high bidder
+    return highBidder.bidAmount;
   }
 
   // This is invoked by anyone to chek if there is are ehters
@@ -77,7 +91,7 @@ contract BiddingContract {
   // Losers will be added to the bidders array ... the claim flag in struct
   // maintains the status of whether the caller has already been given the ethers or not
   function  getClaimAmount() returns(uint){
-    // check if msg.sender in the bidders
+    // check if msg.sender in the bidders`
     // check if claims flag is FALSE
     // return the bid amount
   }
@@ -89,7 +103,7 @@ contract BiddingContract {
     // remove the bidder from the bidders
   }
 
-  // Can a bid end if there are unclaimed ethers 
+  // Can a bid end if there are unclaimed ethers
   // In later version the claims data will be moved to a separate contract
   // Claims will be made losers against the separate contract
   function  canBidEnd() returns (bool) {
@@ -97,7 +111,7 @@ contract BiddingContract {
     // check if the bidding is active i.e., not expired
   }
 
-  // This ends the bidding 
+  // This ends the bidding
   // Only owner can call this function - apply modifier
   // All ethers returned to the owner as part of self destruct
   function endBidding() {
